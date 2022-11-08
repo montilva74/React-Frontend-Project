cb5c92a5ac65737a738eea814e6920e0292e9dc9
Marce_22801 <39542117+MARCELAOJEDA@users.noreply.github.com>
2022-11-08 20:49:48 -0300

    Add files via upload
    Probando, componente claro-oscuro

---
+++ my-ecommerce/Light_Dark/Light_Dark.js
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+import "./Light_Dark.css";
+
+//☀︎ ☽
+export default function Light_Dark() {
+  const [darkMode, setDarkMode] = useState(false);
+  return (
+    <div className={darkMode ? "dark-mode" : "light-mode"}>
+      <div className="container">
+        <span style={{ color: darkMode ? "grey" : "yellow" }}>☀︎</span>
+        <div className="switch-checkbox">
+          <label className="switch">
+            <input type="checkbox" onChange={() => setDarkMode(!darkMode)} />
+            <span className="slider round"> </span>
+          </label>
+        </div>
+        <span style={{ color: darkMode ? "#c96dfd" : "grey" }}>☽</span>
+      </div>
+    </div>
+  );
+}
