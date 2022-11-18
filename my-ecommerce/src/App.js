925363066f751561fcb0bbf8370d9b1f237a0c19
Ariel Perrone <aruerrone@gmail.com>
2022-11-18 19:49:41 -0300

    avance en el footer 18/11

--- my-ecommerce/src/App.js
+++ my-ecommerce/src/App.js
@@ -1,11 +1,26 @@
+import React, { useState} from 'react';
 // import logo from './logo.svg';
 // import './App.css';
-
-import Navbar from "./Componentes/Navbar/Navbar";
+//import Navbar from "./Componentes/Navbar/Navbar";
+import Footer from './Componentes/Footer/Footer.js'
+import Modal from "./Componentes/Modal/Modal.js";
 
 function App() {
+  const [active, setActive] = useState(false);
+
+  const toggle = () => {
+    setActive(!active);
+  }
   return (
-    <Navbar title="Store Fashion Clothes" />
+
+    /*<Navbar title="Store Fashion Clothes" />*/
+    <div className='App'>
+      
+      <Footer/>
+      <Modal active={active} toggle={toggle}>
+        <h1>Hola</h1>
+      </Modal>
+    </div>
   );
 }
 
---
+++ my-ecommerce/src/Componentes/Footer/Footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+import './Footer.css'
+import team8 from './team8.png'
+
+const Footer = () => 
+{
+    return <div className='Footer__box'><p className="Paragraph">Store Fashion Clothes &copy esta hecho por</p><img src={team8} alt='team8' /></div>
+}
+
+export default Footer;
