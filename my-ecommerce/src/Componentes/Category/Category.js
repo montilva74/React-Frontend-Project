97c3e0e530a97294c1041176ba6fc38e255a837b
alba montilva <montilva74@gmail.com>
2022-11-30 20:26:57 -0500

    mostrando products en la vista category

--- my-ecommerce/src/Componentes/Category/Category.js
+++ my-ecommerce/src/Componentes/Category/Category.js
@@ -1,7 +1,10 @@
 import React from 'react'
+import { Col, Container, Row } from 'react-bootstrap';
 import { useParams } from 'react-router-dom'
 import Banner from '../Banner/Banner';
 import TitleCategory from '../TitleCatergory/TitleCategory';
+import ProductItem from '../ProductItem/ProductItem';
+import productos from "./../../Api/products.json";
 
 
 //Aqui se organizan los componentes relacionados con las categorias, para armar la vista categorias.
@@ -17,6 +20,40 @@ export default function Category() {
         <div>
             <TitleCategory name={category}></TitleCategory>
             <Banner imagen={`../../catimages/${ catImages[category] }`}></Banner>
+
+            <Container>
+                <Row className="justify-content-md-center">
+                    <Col xs lg="2">
+                        Filtros
+                    </Col>
+                    <Col xs lg="10">
+                        <div>
+                            Componente de Ordenar
+                        </div>
+                        <div>
+                            <Container>
+                                <Row>
+
+                                { productos.map( item =>
+                                
+
+                                <Col xs lg="3" key="1">
+                                    <ProductItem
+                                        img={item.image}
+                                        name={item.nombre}
+                                        precio={item.price}
+                                    ></ProductItem>
+                                </Col>
+                                
+                                ) }
+
+                                </Row>
+                            </Container>
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
+
         </div>
     )
 }
---
+++ my-ecommerce/src/Componentes/ProductItem/ProductItem.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import "./ProductItem.css";
+
+export default function ProductItem({id, img, name, precio, category}) {
+
+  return (
+    <a href='/producto/id' className='product_a'>
+        <div className='product'>
+            <div className="product_image">
+                <img src={img} alt="producto"/>
+            </div>
+            <div className='product_name'>
+                <a href='/'> { name.substring(0, 20) } </a>
+            </div>
+            <div className='product_price'>
+                ${precio}
+            </div>
+        </div>
+    </a>
+  )
+}
