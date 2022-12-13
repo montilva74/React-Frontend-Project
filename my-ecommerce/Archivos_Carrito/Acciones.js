7955cdb5178f5651c78e431146ecd6dc68808b0b
Marcela Ojeda <queso.en.la.red@gmail.com>
2022-12-13 00:15:31 -0300

    Resumen

---
+++ my-ecommerce/Archivos_Carrito/Acciones.js
@@ -0,0 +1,11 @@
+/* Estas seran las acciones del carrto */
+
+export const TYPES={
+    ADD_TO_CARD:"ADD_TO_CARD", //agregar un producto
+    REMOVE_ONE_FROM_CARD:"REMOVE_ONE_FROM_CARD", //retiralo
+    REMOVE_ALL_FROM_CARD:"REMOVE_ALL_FROM_CARD", // retirar todo lo q tiene el carrito
+    CLEAR_CARD:"CLEAR_CARD",//vaciar carrito
+
+};
+// si qisieramos colocar el total  de unidades del carrito, 
+//lo tendriamos q poner aqui y desarrolarlo en carritoR
