edadf2f7180204a3df735406b12c33b3956a1ba5
Marcela Ojeda <queso.en.la.red@gmail.com>
2022-12-13 00:16:23 -0300

    Resumen de Carrito

---
+++ my-ecommerce/src/Componentes/Carrito/Reumen.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+
+export function Resumen ({subtotal}) {
+
+return <>
+    <h1>Resumen</h1>
+    <h2>Subtotal ${subtotal}.00</h2>
+    <h3>Impuesto $ 0.0</h3>
+
+<button onClick={}>Finalizar Compra</button>
+</>
+}
+
