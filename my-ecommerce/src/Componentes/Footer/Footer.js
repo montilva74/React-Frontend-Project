58517c784369908e180aa8e6d86b220c3a03e5db
Ariel Perrone <aruperrone@gmail.com>
2022-12-14 02:01:28 -0300

    FIN modal_footer, falta colocar un scrollTo

--- my-ecommerce/src/Componentes/Footer/Footer.js
+++ my-ecommerce/src/Componentes/Footer/Footer.js
@@ -4,6 +4,10 @@ import Team8 from './team8.png'
 import Modal from '../Modal/Modal'
 import ModalFooter from "../Modal_Parts/Modal_Footer";
 
+function backTop (){
+    window.scrollX(0)
+}
+
 const Footer = () => 
 {
     const [active, setActive] = useState(false);
@@ -14,7 +18,7 @@ const Footer = () =>
     return  <div className='Footer__box'>
                 <p className="Paragraph">Store Fashion Clothes&copy; hecho por</p>
                 <button className='Footer_Modal__btn' id='boton_modal' onClick={toggle}>
-                    <img className="Team8Img" src={Team8} alt='team8' id='boton_modal' />
+                    <img className="Team8Img" src={Team8} alt='team8' id='boton_modal' onClick={backTop}/>
                 </button>
                 <Modal active={active} toggle={toggle}>
                     <ModalFooter/>
--- my-ecommerce/src/Componentes/Modal_Parts/Modal_Footer.js
+++ my-ecommerce/src/Componentes/Modal_Parts/Modal_Footer.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
 import './Modal_Footer.css';
 import image  from '../../Imagenes/logogithub.png';
+import image2 from '../../Imagenes/mail.png';
 
 export default class ModalFooter extends Component {
-    abrirPagina(url){
-        let win = window.open(url, 'blank');
-        win.focus()
-    };
+    
+   
   render() {
     return (
     <div>
@@ -16,31 +15,34 @@ export default class ModalFooter extends Component {
                 <li className="Modal_Footer__li">
                     <img className="Modal_img__alba" alt=""/>
                     <p className="Modal_Footer__text">Alba Montilva</p>
-                    <a href="mailto:albamontilva@gmail.com"><img alt="mail" className="Modal_Footer__icon"/></a><button onclick={this.abrirPagina("https://github.com/montilva74")}><img alt="" src={image} /></button>
+                    <a href="mailto:albamontilva@gmail.com"><img src={image2} alt="mail" className="Modal_Footer__icon"/></a><a href='https://github.com/montilva74' target='_blank' rel='noreferrer'><img alt="" src={image} /></a>
                 </li>
                 <li className="Modal_Footer__li">
                     <img className="Modal_img__marce" alt=""/>
                     <p className="Modal_Footer__text">Marcela Ojeda</p>
-                    <a href="mailto:claroscuro@gmail.com"><img src="" alt="mail" className="Modal_Footer__icon" /></a>
-                    <button onclick={this.abrirPagina("https://github.com/MARCELAOJEDA")}>
+                    <a href="mailto:claroscuro@gmail.com"><img src={image2} alt="mail" className="Modal_Footer__icon" /></a>
+                    <a href='https://github.com/MARCELAOJEDA' target='_blank' rel='noreferrer' >
                     <img alt="" src={image} />
-                    </button>
+                    </a>
                 </li>
                 <li className="Modal_Footer__li">
                     <img className="Modal_img__ary" alt=""/>
                     <p className="Modal_Footer__text">Ariel Perrone</p>
-                    <a href="mailto:aruperrone@gmail.com"><img src="" alt="mail" className="Modal_Footer__icon" /></a><button onclick={this.abrirPagina('https://github.com/arielperrone')}><img src={image} alt='logogithub'/></button>
+                    <a href="mailto:aruperrone@gmail.com"><img src={image2} alt="mail" className="Modal_Footer__icon" /></a><a href='https://github.com/arielperrone' target='_blank' rel='noreferrer'><img src={image} alt='logogithub'/></a>
                 </li>
             </ul>
             <div className='Modal_Footer__divInferior'>
-                <p className="Modal_Footer__li">Github del proyecto</p>
-                <button onclick={this.abrirPagina('https://github.com/montilva74/React-Frontend-Project')}>
+                <p className="Modal_Footer__text">Github del proyecto</p>
+                <a className="Modal_Footer__divInferior_a" href='https://github.com/montilva74/React-Frontend-Project' target='_blank' rel='noreferrer' >
                     <img src={image} alt="gihub" /> 
-                </button>
+                </a>
 
             </div>
         </div>
+        
     </div>
+
+
     )
   }
 }
