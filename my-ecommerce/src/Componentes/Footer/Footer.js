538e186a8c3a8964d724558b96f1081d20d1cb64
Ariel Perrone <aruperrone@gmail.com>
2022-12-15 21:15:17 -0300

    ARReglando el footer, ya se presetagit add .!

--- my-ecommerce/src/Componentes/Footer/Footer.js
+++ my-ecommerce/src/Componentes/Footer/Footer.js
@@ -15,30 +15,33 @@ const Footer = () =>
             const toggle = () => {
               setActive(!active);
   }     
-    return  
-        <div className="Footer_div_main">
-            <div className="Footer_div_superior">
-            <section className="Footer_section_1">
-                <h2>Suscribite</h2>
-                <div className="Footer_div_newsletter"><input id="Footer_input" type="mail"/><button className="Footer_button" >Suscribite</button></div>
-                <h2>Seguinos</h2>
+    return(
+        <>
+            <div className="Footer_div_main">
+                <div className="Footer_div_superior">
+                <section className="Footer_section_1">
+                    <h2>Suscribite</h2>
+                    <div className="Footer_div_newsletter"><input id="Footer_input" type="mail"/><button className="Footer_button" >Suscribite</button></div>
+                    <h2>Seguinos</h2>
 
-            </section>
-            <section className="Footer_section_2"></section>
-            <section className="Footer_section_3"></section>
+                </section>
+                <section className="Footer_section_2"></section>
+                <section className="Footer_section_3"></section>
 
 
+                </div>
+
+                <div className='Footer__box'>
+                    <p className="Paragraph">Store Fashion Clothes&copy; hecho por</p>
+                    <button className='Footer_Modal__btn' id='boton_modal' onClick={toggle}>
+                        <img className="Team8Img" src={Team8} alt='team8' id='boton_modal' onClick={backTop}/>
+                    </button>
+                    <Modal active={active} toggle={toggle}>
+                        <ModalFooter/>
+                    </Modal>
+                </div>
             </div>
-    
-            <div className='Footer__box'>
-                <p className="Paragraph">Store Fashion Clothes&copy; hecho por</p>
-                <button className='Footer_Modal__btn' id='boton_modal' onClick={toggle}>
-                    <img className="Team8Img" src={Team8} alt='team8' id='boton_modal' onClick={backTop}/>
-                </button>
-                <Modal active={active} toggle={toggle}>
-                    <ModalFooter/>
-                </Modal>
-            </div>
-        </div>
+        </>  
+    )
 }
 export default Footer;
